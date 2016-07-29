8e21664d4521fcf4e2ab87087dba3496b636b41f
Marius Gundersen <me@mariusgundersen.net>
2016-07-29 14:52:04 +0200

    more ES6

--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
-function EventSaga(emitter, options){
-  const store = new Map();
-  let queue = Promise.resolve();
-  async function react(id, reaction, data){
-    let done = false;
-    const realm = {
-      id: id,
-      data: store.get(id),
-      done: () => done = true
-    };
-    reaction.call(realm, data);
-    if(done){
-      store.delete(id);
-    }else{
-      store.set(data.id, realm.data);
+export default class EventSaga{
+  constructor(emitter, options){
+    const store = new Map();
+    let queue = Promise.resolve();
+    async function react(id, reaction, data){
+      let done = false;
+      const realm = {
+        id: id,
+        data: store.get(id),
+        done: () => done = true
+      };
+      reaction.call(realm, data);
+      if(done){
+        store.delete(id);
+      }else{
+        store.set(data.id, realm.data);
+      }
     }
-  }
 
-  function enqueue(handle){
-    return data => {
-      if(!data || typeof(data) !== 'object' || !('id' in data)) return;
+    function enqueue(handle){
+      return data => {
+        if(!data || typeof(data) !== 'object' || !('id' in data)) return;
 
-      queue = queue.then(() => handle(data));
+        queue = queue.then(() => handle(data));
+      };
+    }
+
+    this.on = function(event, reaction){
+      emitter.on(event, enqueue(data => {
+        if(store.has(data.id)){
+          return react(data.id, reaction, data);
+        }
+      }));
+    };
+
+    this.createOn = function(event, reaction){
+      emitter.on(event, enqueue(data => {
+        if(!store.has(data.id)){
+          store.set(data.id, {});
+        }
+        return react(data.id, reaction, data);
+      }));
     };
   }
-
-  this.on = function(event, reaction){
-    emitter.on(event, enqueue(data => {
-      if(store.has(data.id)){
-        return react(data.id, reaction, data);
-      }
-    }));
-  };
-
-  this.createOn = function(event, reaction){
-    emitter.on(event, enqueue(data => {
-      if(!store.has(data.id)){
-        store.set(data.id, {});
-      }
-      return react(data.id, reaction, data);
-    }));
-  };
-};
-
-module.exports = EventSaga;
+}
